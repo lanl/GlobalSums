82dcb4b5c50d9f8825218cb29684fa3258fa094f
brobey <brobey@lanl.gov>
2019-08-09 13:13:31 -0600

    Adding more sums with remainder for fog

---
+++ knuth_fog_vector8_remainder.cpp
@@ -0,0 +1,58 @@
+#include "vectorclass.h"
+
+static double sum[8] __attribute__ ((aligned (64)));
+
+extern "C" {
+double do_knuth_sum_agner_v8(double* var, long ncells);
+}
+
+double do_knuth_sum_agner_v8(double* var, long ncells)
+{
+   Vec8d local_sum(0.0);
+   Vec8d local_correction(0.0);
+   Vec8d var_v;
+
+   int ncells_main=(ncells/8)*8;
+   int ncells_remainder=ncells%8;
+   for (long i = 0; i < ncells_main; i+=8) {
+      var_v.load(var+i);
+      Vec8d u = local_sum;
+      Vec8d v = var_v + local_correction;
+      Vec8d upt = u + v;
+      Vec8d up = upt - v;
+      Vec8d vpp = upt - up;
+      local_sum = upt;
+      local_correction = (u - up) + (v - vpp);
+   }
+   if (ncells_remainder > 0) {
+      var_v.load_partial(ncells_remainder,var+ncells_main);
+      Vec8d u = local_sum;
+      Vec8d v = var_v + local_correction;
+      Vec8d upt = u + v;
+      Vec8d up = upt - v;
+      Vec8d vpp = upt - up;
+      local_sum = upt;
+      local_correction = (u - up) + (v - vpp);
+   }
+
+
+   Vec8d sum_v = local_sum + local_correction;
+   sum_v.store(sum);
+
+   // double to do final sum
+   double ud, vd, uptd, upd, vppd;
+   double final_sum = 0.0;
+   double final_correction = 0.0;
+
+   for (long i = 0; i < 8; i++) {
+      ud = final_sum;
+      vd = sum_v[i] + final_correction;
+      uptd = ud + vd;
+      upd = uptd - vd;
+      vppd = uptd - upd;
+      final_sum = uptd;
+      final_correction = (ud - upd) + (vd - vppd);
+   }
+
+   return(final_sum);
+}
---
+++ knuth_fog_vector_remainder.cpp
@@ -0,0 +1,58 @@
+#include "vectorclass.h"
+
+static double sum[4] __attribute__ ((aligned (64)));
+
+extern "C" {
+double do_knuth_sum_agner_v(double* var, long ncells);
+}
+
+double do_knuth_sum_agner_v(double* var, long ncells)
+{
+   Vec4d local_sum(0.0);
+   Vec4d local_correction(0.0);
+   Vec4d var_v;
+
+   int ncells_main=(ncells/4)*4;
+   int ncells_remainder=ncells%4;
+   for (long i = 0; i < ncells_main; i+=4) {
+      var_v.load(var+i);
+      Vec4d u = local_sum;
+      Vec4d v = var_v + local_correction;
+      Vec4d upt = u + v;
+      Vec4d up = upt - v;
+      Vec4d vpp = upt - up;
+      local_sum = upt;
+      local_correction = (u - up) + (v - vpp);
+   }
+   if (ncells_remainder > 0) {
+      var_v.load_partial(ncells_remainder,var+ncells_main);
+      Vec4d u = local_sum;
+      Vec4d v = var_v + local_correction;
+      Vec4d upt = u + v;
+      Vec4d up = upt - v;
+      Vec4d vpp = upt - up;
+      local_sum = upt;
+      local_correction = (u - up) + (v - vpp);
+   }
+
+
+   Vec4d sum_v = local_sum + local_correction;
+   sum_v.store(sum);
+
+   // double to do final sum
+   double ud, vd, uptd, upd, vppd;
+   double final_sum = 0.0;
+   double final_correction = 0.0;
+
+   for (long i = 0; i < 4; i++) {
+      ud = final_sum;
+      vd = sum_v[i] + final_correction;
+      uptd = ud + vd;
+      upd = uptd - vd;
+      vppd = uptd - upd;
+      final_sum = uptd;
+      final_correction = (ud - upd) + (vd - vppd);
+   }
+
+   return(final_sum);
+}
